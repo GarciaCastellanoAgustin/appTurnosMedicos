7246f5ebb4fd3e579abe9487dd70283b7bc494bd
GarciaCastellanoAgustin <73252026+GarciaCastellanoAgustin@users.noreply.github.com>
2021-09-13 11:50:49 -0300

    commiteamos  cambios esteticos y funcionalidades

--- medicalApp/login.php
+++ medicalApp/login.php
@@ -10,82 +10,59 @@
 
 <body>
 
-    <section class="vh-100">
-  <div class="container-fluid h-custom">
-    <div class="row d-flex justify-content-center align-items-center h-100">
+<section>
+  <div class="container-fluid">
+    <div class="row d-flex justify-content-center align-items-center">
+
       <div class="col-md-9 col-lg-6 col-xl-5">
         <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-login-form/draw2.png" class="img-fluid"
           alt="Sample image">
       </div>
+
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
         <form action="validar.php" method="post">
 
-          <div class="divider d-flex align-items-center my-4">
-            <p class="text-center fw-bold mx-3 mb-0">Or</p>
-          </div>
-
-          <!-- Email input -->
+          <!-- Mail -->
           <div class="form-outline mb-4">
 
           <input type="text" placeholder="Ingresar mail" name="usuario"/>
-          <label class="form-label" for="form3Example3">Email address</label>
+          <label class="form-label">Ingresar Usuario</label>
+
           </div>
 
-          <!-- Password input -->
+          <!-- Pass -->
           <div class="form-outline mb-3">
           <input type="password" placeholder="Ingresar contraseña" name="password"/>
             <label class="form-label" for="form3Example4">Password</label>
           </div>
 
+          <!-- Checkbox -->
           <div class="d-flex justify-content-between align-items-center">
-            <!-- Checkbox -->
+            
             <div class="form-check mb-0">
-              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-              <label class="form-check-label" for="form2Example3">
-                Remember me
+              <input class="form-check-input me-2" type="checkbox" value=""/>
+              <label class="form-check-label">
+                un estoy roto
               </label>
             </div>
-            <a href="#!" class="text-body">Forgot password?</a>
+
           </div>
 
+          <!--Register-->
           <div class="text-center text-lg-start mt-4 pt-2">
+
             <button type="button" class="btn btn-primary btn-lg"
               style="padding-left: 2.5rem; padding-right: 2.5rem;"><input type="submit" value="Ingresar"></button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                class="link-danger">Register</a></p>
+            <p class="small fw-bold mt-2 pt-1 mb-0">No tenes cuenta? <a href="#!"
+                class="link-danger">Registrarse</a></p>
+
           </div>
 
         </form>
       </div>
     </div>
   </div>
-  <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-    <!-- Copyright -->
-    <div class="text-white mb-3 mb-md-0">
-      Copyright © 2020. All rights reserved.
-    </div>
-    <!-- Copyright -->
-
-    <!-- Right -->
-    <div>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="#!" class="text-white">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
-    </div>
-    <!-- Right -->
-  </div>
 </section>
-
-
 
 </body>
 </html>
